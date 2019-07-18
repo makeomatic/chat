1002c90511ad68f1caf9203f2dd978e8c9cc4065
afoninsky <vkfont@yandex.ru>
2019-07-18 13:03:48 +0300

    tune keepalive parameters

--- server/hdl_grpc.go
+++ server/hdl_grpc.go
@@ -126,16 +126,16 @@ func serveGrpc(addr string, tlsConf *tls.Config) (*grpc.Server, error) {
 	}
 
 	kepConfig := keepalive.EnforcementPolicy{
-		MinTime:             1 * time.Second, // If a client pings more than once every second, terminate the connection
-		PermitWithoutStream: true,            // Allow pings even when there are no active streams
+		MinTime:             1 * time.Second,
+		PermitWithoutStream: true,
 	}
 	
 	kpConfig := keepalive.ServerParameters{
-		MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
-		MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
-		MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
-		Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
-		Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
+		// MaxConnectionIdle:     15 * time.Second,
+		// MaxConnectionAge:      30 * time.Second,
+		// MaxConnectionAgeGrace: 5 * time.Second,
+		Time:                  60 * time.Second,
+		Timeout:               20 * time.Second,
 	}
 	opts = append(opts, grpc.KeepaliveEnforcementPolicy(kepConfig))
 	opts = append(opts, grpc.KeepaliveParams(kpConfig))
